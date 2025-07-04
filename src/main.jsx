a227c63a154faed2d4c9ab0822db3adf5306ec4f
Dinesh Kumar Verma <dular88@gmail.com>
2025-07-04 12:47:33 +0530

    Updated main.jsx file with hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // ✅ important
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      {" "}
+      {/* 👈 wrap App in HashRouter for GitHub Pages */}
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
